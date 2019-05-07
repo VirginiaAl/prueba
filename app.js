1041ef7662868a464cb11b31ba47ac198cc2a242
Virginia Álvarez <virginiaalvarez@MacBook-Pro-de-Virginia.local>
2019-05-07 13:12:53 +0200

    steps

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function getWeather(city) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ city },ES&APPID=4102b1f13a4cd8fab6424c32c2cf63b8`)
+
     .then(resp => {
       return resp.json()
     })
@@ -12,11 +13,17 @@ function getWeather(city) {
     })
   }
 
-  document.getElementById('button').addEventListener('click', () => {
-    let city = document.getElementById('search').value;
-    //document.getElementById('location').innerHTML = city;
+   document.getElementById('button').addEventListener('click', () => {
+     let city = document.getElementById('search').value;
+
     getWeather(city);
   })
+
+// const object = new Object {
+//   this.location = location,
+//   this.temp = temp,
+//   this.description = description,
+//
 
 function draw(o) {
 
@@ -25,4 +32,12 @@ let celsius = Math.round(parseFloat(o.main.temp)-273,15);
 document.getElementById('temp').innerHTML = celsius +  'ºC';
 document.getElementById('icon').src = `http://openweathermap.org/img/w/${o.weather[0].icon}.png`;
 document.getElementById('description').innerHTML = o.weather[0].description;
+document.getElementById('speed').innerHTML = o.wind.speed;
+document.getElementById('humidity').innerHTML = o.main.humidity;
 }
+
+let map = 'https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=4102b1f13a4cd8fab6424c32c2cf63b8'
+
+
+
+//avr();
